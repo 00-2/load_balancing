6b927ceeaa4c05b9a9a56bd26a85976e33711101
00-2 <9x02@mail.ru>
2022-06-28 14:19:03 +0300

    Create async handle request

--- balancier/backend/server.py
+++ balancier/backend/server.py
@@ -1,8 +1,10 @@
 from array import array
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 import os
 import Pod
 import Connection
+
+import asyncio
 
 env = os.environ.get("APP_ENV", "dev")
 print(f"Starting application in {env} mode")
@@ -15,18 +17,40 @@ def config_pods() -> array:
         for i in range(1,COUNT_OF_PODS)
     ]
 
+
+async def fetch_html(url: str) -> str:
+    resp = await request(method="GET", url=url)
+    resp.raise_for_status()
+    html = await resp.text()
+    return html
+
+async def handle_request(pod, connection) -> None:
+    pod.create_connection()
+    connection.set_pod_url(pod.url)
+    html = await fetch_html(pod.url)
+    if (html):
+        connection.set_is_finalized()
+
+    #TODO async function, wait to response +
+
 class Balancier(Flask):
     _next = 0
-    connections = []
+    # pool of connections
+    connections = [Connection()] * 10000
     pods = config_pods()
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     def round_robin(self):
         for number in self.pods:
             pod = self.pods[number]
-            if pod["is_active"]:
-                #TODO send redirect 
-                redirect()
+            if pod.is_active:
+                connection = Connection()
+                try:
+                    handle_request(pod, connection)
+                except:
+                    #TODO handle request fail
+                    pass
+                redirect(pod.url)
 
 
 app = Balancier(f"client")
