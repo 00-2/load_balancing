eaeb24de809e2fa89e1a282b0c409235aa7b9019
00-2 <9x02@mail.ru>
2022-06-28 11:48:29 +0300

    docker build client  server

--- backend/backend/server.py
+++ backend/backend/server.py
@@ -14,12 +14,12 @@ class Client(Flask):
 
 app = Client(f"client")
 
-app.config.from_object(f"backend.{env}_settings")
+# app.config.from_object(f"backend.{env}_settings")
+app.server_id = 1
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return app.server_id
-
+    return str(app.server_id)
 
 if __name__ == '__main__':
     app.run()
